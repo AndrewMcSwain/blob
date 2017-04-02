657120c9c278475f4611c86b9842912b31254af3
Vishnu Airy (Aero-vision) <andrew@e4b.us>
2017-04-02 18:08:41 -0300

    Create a.js

---
+++ a.js
@@ -0,0 +1,23 @@
+# blob
+function domainType(domains) {
+var domainTypeMap ='' 
+    {
+        'org' : 'organization',
+        'com' : 'commercial',
+        'net' : 'network',
+        'info' : 'information'
+    };
+
+function getTail(item){
+    var isDot = '/['.']/g'
+    var len = this.item.length;
+    var index = len;
+    var splitlength;
+    if (item.charAt(index) !=== isDot){
+        index--;
+    else
+        substrlength= length - index;
+    }
+    var suffix = item.substr(index, substrlength)
+}
+    
